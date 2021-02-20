476a9c6659d31962a4d90f197e8652573c90a2cc
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-02-20 00:07:25 +0000

    v1 multiple pages oop

--- app.py
+++ app.py
@@ -13,21 +13,27 @@ class Prod:
 @app.route('/')
 def hello():
     # Make a request to https://codedamn-classrooms.github.io/webscraper-python-codedamn-classroom-website/
-    page = requests.get('https://svens.is/collections/frontpage')
-    soup = BeautifulSoup(page.content, 'html.parser')
-
+    end_page_num = 3
+    i = 1
     myList = []
-    # products = ""
-    productItem = soup.find_all("div", {"class": "product-card"})
-    for tag in productItem:
-        tdTags = tag.find_all("a", {"class": "grid-view-item__link"})
-        for t in tdTags:
-            x = t.find_all("span", {"class": "visually-hidden"})
-            for s in x:
-                myList.append( Prod(s.text))
+    while i <= end_page_num:
+        page = requests.get('https://svens.is/collections/frontpage?page={}'.format(i))
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        
+        # products = ""
+        productItem = soup.find_all("div", {"class": "product-card"})
+        for tag in productItem:
+            tdTags = tag.find_all("a", {"class": "grid-view-item__link"})
+            for t in tdTags:
+                x = t.find_all("span", {"class": "visually-hidden"})
+                for s in x:
+                    myList.append(Prod(s.text))
+        i+=1
 
     for obj in myList:
         print(obj.name) 
+    
 
     return ""
 
