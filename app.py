2b035c33a8369a74959ea1226f7d5ade4a3dd69f
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-02-20 00:55:45 +0000

    v1 print rdy

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 class Prod:
-    def __init__(self, name):  
+    def __init__(self, name, vendor, price):  
         self.name = name  
-
-
+        self.vendor = vendor
+        self.price = price
 
 @app.route('/')
 def hello():
@@ -22,17 +22,34 @@ def hello():
 
         
         # products = ""
-        productItem = soup.find_all("div", {"class": "product-card"})
-        for tag in productItem:
-            tdTags = tag.find_all("a", {"class": "grid-view-item__link"})
-            for t in tdTags:
-                x = t.find_all("span", {"class": "visually-hidden"})
-                for s in x:
-                    myList.append(Prod(s.text))
+        # productItem = soup.find_all("div", {"class": "product-card"})
+        # for tag in productItem:
+        #     tdTags = tag.find_all("a", {"class": "grid-view-item__link"})
+        #     for t in tdTags:
+        #         x = t.find_all("span", {"class": "visually-hidden"})
+        #         for s in x:
+        #             myList.append(Prod(s.text))
         i+=1
 
-    for obj in myList:
-        print(obj.name) 
+        for productContainer in soup.find_all("div",{"class": "grid-view-item product-card"}):
+
+            productItem = productContainer.find("div", {"class": "h4 grid-view-item__title product-card__title"})
+            print(productItem.get_text(strip=True, separator='\n'))
+
+            productVendor = productContainer.find("div", {"class": "price__vendor price__vendor--listing"})
+            print((productVendor.get_text(strip=True, separator='\n')))
+            
+            productPrice = productContainer.find("span", {"class": "price-item price-item--regular"})
+            print(productPrice.get_text(strip=True, separator='\n'))
+
+            print("\n")
+
+        
+        
+        # print(productItem)
+
+    # for obj in myList:
+    #     print(obj.name) 
     
 
     return ""
